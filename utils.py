31f65bd62e4e9c8689b7f97fa69c6f1672f14b60
erwan.lemattre <erwan.lemattre@universite-paris-saclay.fr>
2024-03-02 12:23:11 +0100

    Co-authored-by: YannisCHUPIN <YannisCHUPIN@users.noreply.github.com>

--- utils.py
+++ utils.py
@@ -147,3 +147,164 @@ def analyse_bi_quali_quanti(quali, quanti, df):
 
   hist_prop = px.histogram(df, x=quanti, color=quali, barmode='overlay', opacity=0.75, histnorm="probability")
   hist_prop.show()
+
+
+
+  #####################################################################################################
+  #####################################################################################################
+  #                                     Fonctions One Hot Encoding                                    #
+  #####################################################################################################
+  #####################################################################################################
+
+
+  def oneHotShapingColumns(indicesCat, name, x) :
+    indiceTrue = int(name[-1])
+    zeroes = []
+    uns = []
+    for key in indicesCat.keys() :
+        if indicesCat[key] == indiceTrue :
+            uns.append(key)
+        else :
+            zeroes.append(key)
+    x[name].replace(to_replace=zeroes, value=0, inplace=True)
+    x[name].replace(to_replace=uns, value=1, inplace=True)
+
+"""
+### Lum :
+1. Plein jour
+2. Crépuscule ou aube
+3. Nuit sans éclairage (fusion 3-4)
+4. Nuit avec éclairage public allumé (ancien 5)
+"""
+def oneHotLum(x) :
+    indicesCat = {1:1, 2:2, 3:3, 4:3, 5:4}
+    for i in range(1, 5) :
+        x['lum'+str(i)] = x['lum']
+        oneHotShapingColumns(indicesCat, 'lum'+str(i), x)
+    x.drop(columns=['lum'], inplace=True)
+
+
+"""
+### Col - Type de collision :
+1. Deux véhicules - frontale
+2. Deux véhicules – par l’arrière 
+3. Deux véhicules – par le coté
+4. Trois véhicules et plus – en chaîne
+5. Trois véhicules et plus - collisions multiples 
+6. Autre collision
+"""
+def oneHotCol(x) :
+    indicesCat = {1:1, 2:2, 3:3, 4:4, 5:5, 6:6}
+    for i in range(1, 7) :
+        x['col'+str(i)] = x['col']
+        oneHotShapingColumns(indicesCat, 'col'+str(i), x)
+    x.drop(columns=['col'], inplace=True)
+
+
+"""
+### VMA - Vitesse sur les lieux :
+1. v< 50
+2. v>=50 && v<=70
+3. v> 70 && v< 100
+4. v>=100
+"""
+def oneHotVma(x) :
+    indicesCat = {1:1, 2:2, 3:3, 4:4}
+    for i in range(1, 5) :
+        x['vma'+str(i)] = x['vma']
+        oneHotShapingColumns(indicesCat, 'vma'+str(i), x)
+    x.drop(columns=['vma'], inplace=True)
+
+"""
+### trajet :
+1. Domicile – travail 
+2. Domicile – école 
+3. Courses – achats
+4. Utilisation professionnelle 
+5. Promenade – loisirs
+"""
+def oneHotTrajet(x) :
+    indicesCat = {-1:0, 0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 9:0}
+    for i in range(1, 6) :
+        x['trajet'+str(i)] = x['trajet']
+        oneHotShapingColumns(indicesCat, 'trajet'+str(i), x)
+    x.drop(columns=['trajet'], inplace=True)
+
+"""
+### situ - description du lieu de l'accident :
+1. Sur chaussée (ultra majotitaire)
+2. Sur bande d’arrêt d’urgence 
+3. Sur accotement
+4. Sur trottoir
+5. Sur piste cyclable
+"""
+def oneHotSitu(x) :
+    indicesCat = {-1:0, 0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:0, 8:0}
+    for i in range(1, 6) :
+        x['situ'+str(i)] = x['situ']
+        oneHotShapingColumns(indicesCat, 'situ'+str(i), x)
+    x.drop(columns=['situ'], inplace=True)
+
+
+"""
+### atm - Condditions atmosphériques
+1. Normale
+2. Pluie légère 
+3. Pluie forte
+4. Neige - grêle
+5. Brouillard - fumée 
+6. Vent fort - tempête 
+7. Temps éblouissant 
+8. Temps couvert
+"""
+def oneHotAtm(x) :
+    indicesCat = {-1:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:0}
+    for i in range(1, 9) :
+        x['atm'+str(i)] = x['atm']
+        oneHotShapingColumns(indicesCat, 'atm'+str(i), x)
+    x.drop(columns=['atm'], inplace=True)
+
+
+"""
+### catv - catégorie du véhicule:
+1. Bicyclette
+2. Cyclomoteur <50cm3
+3. VL seul
+4. Utilitaire
+5. Motocyclette > 125 cm3
+"""
+def oneHotCatv(x) :
+    indicesCat = {0:0, 1:1, 2:2, 3:3, 4:4, 5:5}
+    for i in range(1, 6) :
+        x['catv'+str(i)] = x['catv']
+        oneHotShapingColumns(indicesCat, 'catv'+str(i), x)
+    x.drop(columns=['catv'], inplace=True)
+
+"""
+### choc - type de choc enduré si enduré:
+1. avant
+2. arrière
+3. coté
+4. multiple
+"""
+def oneHotChoc(x) :
+    indicesCat = {-1:0, 0:0, 1:1, 2:1, 3:1, 4:2, 5:2, 6:2, 7:3, 8:3, 9:4}
+    for i in range(1, 10) :
+        x['choc'+str(i)] = x['choc']
+        oneHotShapingColumns(indicesCat, 'choc'+str(i), x)
+    x.drop(columns=['choc'], inplace=True)
+
+"""
+### surf - Etat de la surface :
+1. Normale
+2. Humide
+3. Inondée
+4. gel/neige
+5. grasse
+"""
+def oneHotSurf(x) :
+    indicesCat = {-1:0, 1:1, 2:2, 3:2, 4:3, 5:4, 6:5, 7:4, 8:5, 9:0}
+    for i in range(1, 9) :
+        x['surf'+str(i)] = x['surf']
+        oneHotShapingColumns(indicesCat, 'surf'+str(i), x)
+    #x.drop(columns=['surf'], inplace=True)
